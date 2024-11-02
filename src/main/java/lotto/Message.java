ebd7e387bf1d2fe3edc4cb0e9e88b3b2250a1309
jungsehui <jungsehui@naver.com>
2024-11-02 22:18:10 +0900

    feat(Message.java): 기본 출력을 위한 Enum 메시지 작성

--- src/main/java/lotto/Message.java
+++ src/main/java/lotto/Message.java
@@ -1,4 +1,20 @@
 package lotto;
 
 public enum Message {
+
+    PURCHASE_PRICE("구입금액을 입력해 주세요."),
+    PURCHASE_QUANTITY("개를 구매했습니다."),
+    WINNING_NUMBERS("\n당첨 번호를 입력해 주세요."),
+    BONUS_NUMBER("\n보너스 번호를 입력해 주세요."),
+    WINNING_STATISTICS("\n당첨 통계");
+
+    private final String message;
+
+    Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
