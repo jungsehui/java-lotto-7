8718776ed88db382a2b5594abb4eaea6770f90eb
jungsehui <jungsehui@naver.com>
2024-11-02 22:46:27 +0900

    feat(Application.java): 로또 구입 금액 입력에 따라 구매 수량을 자동으로 결정하고 출력하는  기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,9 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
@@ -10,6 +13,16 @@ public class Application {
 
         if (Integer.parseInt(inputPurchasePrice) % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 잘못된 값을 입력하였습니다. 구입 금액은 1,000원 단위로 입력해 주세요");
+        }
+
+        int purchaseQuantity = Integer.parseInt(inputPurchasePrice) / 1000;
+        System.out.println("\n" + purchaseQuantity + Message.PURCHASE_QUANTITY.getMessage());
+
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < purchaseQuantity; i++) {
+            Lotto lotto = new Lotto(Lotto.create());
+            System.out.println(lotto.getNumbers());
+            lottos.add(lotto);
         }
     }
 }
