fc83c7bb0dbfc7c48a8f4f9e4a9fc1d831f20c95
jungsehui <jungsehui@naver.com>
2024-11-02 22:42:45 +0900

    feat(Application.java): 로또 구입 금액을 입력받고 1,000원 단위로 검증하도록 하는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println(Message.PURCHASE_PRICE.getMessage());
+        String inputPurchasePrice = Console.readLine();
+
+        if (Integer.parseInt(inputPurchasePrice) % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 값을 입력하였습니다. 구입 금액은 1,000원 단위로 입력해 주세요");
+        }
     }
 }
